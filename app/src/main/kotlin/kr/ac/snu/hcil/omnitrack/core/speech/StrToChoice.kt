0e747fc43dcc49d60124e692bac3f49389385c17
mrswagbhinav <abhinav.vedmala@gmail.com>
2021-04-21 13:54:57 -0400

    Choice inputs now correctly processed for single digit numbers in english word and arabic numbers

--- app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/core/speech/StrToChoice.kt
+++ app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/core/speech/StrToChoice.kt
@@ -23,12 +23,12 @@ class StrToChoice(inputStr: String){
 
         println("choice entry 1: ${entries!![1].text}, size: $size")
 
-        if(size != 0){
+        if (size != 0){
             for (entry in entries){
                 println("choice entry: $entry")
                 // need to revisit this, there're might be some other ambiguous matching cases
-                if(inputStr.contains(entry.text, true) || entry.text.contains(inputStr, true)){
-                    if(!selectedIndex.contains(entry.id)){
+                if (isMatch(inputStr, entry.text)) {
+                    if (!selectedIndex.contains(entry.id)){
                         if(!multiChoice!!){
                             selectedIndex.clear()
                         }
@@ -40,6 +40,31 @@ class StrToChoice(inputStr: String){
             println("choice iterator size: $size")
         }
             return selectedIndex.toIntArray()
+    }
+
+    fun getSynonym (inputStr: String): String {
+        var str = inputStr
+        val digits = "0123456789"
+        val strDigits: Array<String> = arrayOf("zero", "one", "two", "three", "four", "five", "six",
+                "seven", "eight", "nine")
+
+        for (i in 0 until str.length - 1) {
+            if (digits.contains(str[i])) {
+                str = str.replace(str.substring(i, i+1), strDigits[str[i] - '0'], true)
+            }
+        }
+
+        return str
+    }
+
+    fun isMatch (str1: String, str2: String): Boolean {
+        val str1Synonym = getSynonym(str1)
+        val str2Synonym = getSynonym(str2)
+
+        return (str1.contains(str2, true) || str2.contains(str1, true) ||
+                str1Synonym.contains(str2, true) || str2.contains(str1Synonym, true) ||
+                str1.contains(str2Synonym, true) || str2Synonym.contains(str1, true) ||
+                str1Synonym.contains(str2Synonym, true) || str2Synonym.contains(str1Synonym, true))
     }
 
 }
